72d11fb94f0e12d3e1c01ca1b43c5d96c98a1d18
Cristobal Barberis <cristobal@barberis.com>
2022-07-16 19:13:28 -0400

    current forecast

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -2,16 +2,15 @@ var cityWeatherDivEl = document.querySelector("#city-weather");
 var date = moment().format("M/DD/YYYY");
 var city = "San Diego";
 
-var apiCurrentData = [
-                      {"temp": "current.temp"},
-                      {"humidity": "current.humidity"},
-                      {"wind": "current.wind_speed"},
-                      {"uvi": "current.uvi"}
-                    ];
-var apiForecastData = [
-                      {"temp": "daily._day_.temp.day"},
-                      {"humidity": "daily._day_.humidity"},
-                      {"wind": "daily._day_.wind_speed"}
+var currentApiPath = {"Temp": "current.temp",
+                      "Wind": "current.wind_speed",
+                      "Humidity": "current.humidity",
+                      "UV Index": "current.uvi"
+                      };
+var forecastApiPath = [
+                      {"Temp": "daily._day_.temp.day"},
+                      {"Humidity": "daily._day_.humidity"},
+                      {"Wind": "daily._day_.wind_speed"}
                     ];
 
 
@@ -36,7 +35,7 @@ var getWether = function (coord) {
     if(response.ok){
       response.json().then(function (data) {
         console.log(data);
-        createCityDetailElenments(data);
+        createCurrent(data);
       });
 
     }
@@ -44,72 +43,97 @@ var getWether = function (coord) {
   
 }
 
-var createCityDetailElenments = function(data) {
-  var parentEl = null;
-  for (let i = 0; i < 5; i++) {
-    if (i === 0) {
-      // create today title elements
-      var todayWatherDiv = document.createElement("div");
-      todayWatherDiv.classList.add("city-detail", "mb-4");
-      var cityTitleDivEl = document.createElement("div");
-      var cityTitleH2El = document.createElement("h2");
-      cityTitleH2El.textContent = city + " (" + date + ")";
-      cityTitleDivEl.appendChild(cityTitleH2El);
-      todayWatherDiv.appendChild(cityTitleDivEl);
-      cityWeatherDivEl.appendChild(todayWatherDiv);
-      parentEl = todayWatherDiv;
+// create current weather 
+var createCurrent = function(data) {
+    var todayWatherDiv = document.createElement("div");
+    todayWatherDiv.classList.add("city-detail", "mb-4");
+    var cityTitleDivEl = document.createElement("div");
+    var cityTitleH2El = document.createElement("h2");
+    cityTitleH2El.textContent = city + " (" + date + ")";
+    cityTitleDivEl.appendChild(cityTitleH2El);
+    todayWatherDiv.appendChild(cityTitleDivEl);
+    cityWeatherDivEl.appendChild(todayWatherDiv);
+    todayWatherDiv.appendChild(getWeatherMetrics(data, currentApiPath))
 
-//     } //delete
-      // parentEl = todayWatherDiv;
-    }else{
-      if(i === 1) {
-        // create forecast title element
-        var foreTitleDivEl = document.createElement("div");
-        foreTitleDivEl.classList.add("city-forecast");
-        var foreTitleH3El = document.createElement("h3");
-        foreTitleH3El.textContent = "5-Day Forecast";
-        foreTitleDivEl.appendChild(foreTitleH3El);
-      }
-    }
-    //   // create forecast date element
-    //   var foreFlexDivEl = document.createElement("div");
-    //   foreFlexDivEl.classList.add("row d-flex");
-    //   var foreCardDivEl = document.createElement("div");
-    //   foreCardDivEl.classList.add("col card");
-    //   var foreDateH4El = document.createElement("h4");
-    //   foreDateH4El.textContent = date;
-    //   foreCardDivEl.appendChild(foreDateH4El);
-    //   var foreImgDivEl = document.createElement("div");
-    //   var foreImgEl = document.createElement("img");
-    //   foreImgDivEl.appendChild(foreImgEl);
-    //   foreCardDivEl.appendChild(foreImgDivEl);
-    //   foreFlexDivEl.appendChild(foreCardDivEl);
-    //   foreTitleDivEl.appendChild(foreFlexDivEl);
+    createForecast(data);
+
+
+      
+    
+  
     // }
     // create detail elements
 
-  }
+  
  
 //   var metrics = getWeatherMetrics(data);
 //   parentEl.appendChild(metrics);
 
 
 //   // next days
-//   date = moment(date, "M/DD/YYYY").add(1, 'days');
-// }
+}
+// display current weather
+var createForecast = function () {
+  var foreTitleDivEl = document.createElement("div");
+  foreTitleDivEl.classList.add("city-forecast");
+  var foreTitleH3El = document.createElement("h3");
+  foreTitleH3El.textContent = "5-Day Forecast";
+  foreTitleDivEl.appendChild(foreTitleH3El);
+  cityWeatherDivEl.appendChild(foreTitleDivEl);
+  var foreFlexDivEl = document.createElement("div");
+  foreFlexDivEl.classList.add("row", "d-flex");
+  foreTitleDivEl.appendChild(foreFlexDivEl);
 
-// var getWeatherMetrics = function (data) {
-//   var cityDetailDivEl = document.createElement("div");
-//   var cityUlEl = document.createElement("ul");
-//   for (let i = 0; i < weatherMetrics.length; i++) {
-//     var metrics = weatherMetrics[i];
-//     var cityTempliEl = document.createElement("li");
-//     cityTempliEl.textContent = data.main.temp;
-//     cityUlEl.appendChild(cityTempliEl);
-//   }
-//   cityDetailDivEl.appendChild(cityUlEl);
+  foreCardDivEl = [];
+  foreDateH4El = [];
+  foreImgDivEl = [];
+  foreImgEl = [];
 
-//   return cityDetailDivEl;
+  for (let i = 0; i < 5; i++) {
+    // create forecast date element
+    foreCardDivEl[i] = document.createElement("div");
+    foreCardDivEl[i].classList.add("col", "card");
+    foreDateH4El[i] = document.createElement("h4");
+    foreDateH4El[i].textContent = date;
+    foreCardDivEl[i].appendChild(foreDateH4El[i]);
+    foreImgDivEl[i] = document.createElement("div");
+    foreImgEl[i] = document.createElement("img");
+    foreImgDivEl[i].appendChild(foreImgEl[i]);
+    foreCardDivEl[i].appendChild(foreImgDivEl[i]);
+    foreFlexDivEl.appendChild(foreCardDivEl[i]);
+    // todayWatherDiv.appendChild(getWeatherMetrics(data, currentApiPath))
+    date = moment(date, "M/DD/YYYY").add(1, 'days').format("M/DD/YYYY");
+
+  }
+  // cityWeatherDivEl.appendChild(foreFlexDivEl);
+}
+
+var apiPathVal = function (data, keys) {
+  return keys.split('.').reduce(function (cur, key) {
+      return cur[key];
+  }, data);
+};
+  
+
+var getWeatherMetrics = function (data, apiMetrics) {
+  var cityDetailDivEl = document.createElement("div");
+  var cityUlEl = document.createElement("ul");
+  var liEl = [];
+  var spanEl = [];
+  for (metric in apiMetrics) {
+    liEl[metric] = document.createElement("li");
+    spanEl[metric] = document.createElement("span");
+    if (metric === "UV Index") {
+      spanEl[metric].classList.add("badge", "badge-success");
+    }
+    liEl[metric].textContent = metric + ": ";
+    spanEl[metric].textContent = apiPathVal(data, apiMetrics[metric]); 
+    liEl[metric].appendChild(spanEl[metric]);
+    cityUlEl.appendChild(liEl[metric]);
+  }
+  cityDetailDivEl.appendChild(cityUlEl);
+
+  return cityDetailDivEl;
 
 }
 getCityCoords(city);
